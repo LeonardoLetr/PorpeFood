14734b2d86278b9187d5d6e38812ad6530778699
LeonardoLetr <leo.rbg26@gmail.com>
2020-08-25 22:26:36 -0300

    Start configuration

---
+++ app/src/main/java/com/uniso/lpdm/porpefood/MainActivity.java
@@ -0,0 +1,15 @@
+package com.uniso.lpdm.porpefood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    /* Leonardo Rodrigues RA:00101457 Ciências da Computação*/
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
