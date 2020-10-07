fa1b4e5f61b88c75ec7c5ded3a43325c22eda61b
leorb <leo.rbg26@gmail.com>
2020-10-06 22:33:33 -0300

    [Aula][8]-Criando um banco com sqlite para inserir os produtos

---
+++ app/src/main/java/com/uniso/lpdm/porpefood/DatabaseHelper.java
@@ -0,0 +1,56 @@
+package com.uniso.lpdm.porpefood;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "DBPorpeFood";
+    private static final int DB_VERSION = 1;
+
+    DatabaseHelper(Context ctx) {
+        super(ctx, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        updateDB(db, 0, DB_VERSION);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        updateDB(db, oldVersion, newVersion);
+    }
+
+    public static void insertOrder(SQLiteDatabase db, String name, String description, double unit_price, int image_resource_id) {
+        ContentValues newOrder = new ContentValues();
+        newOrder.put("name", name);
+        newOrder.put("description", description);
+        newOrder.put("unit_price", unit_price);
+        newOrder.put("image_resource_id", image_resource_id);
+
+        db.insert("Order", null, newOrder);
+    }
+
+    private void updateDB(SQLiteDatabase db,  int oldVersion, int newVersion) {
+        String sql;
+
+        if (oldVersion < 1) {
+            sql =   "CREATE TABLE Request (" +
+                    "_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                    "name TEXT, " +
+                    "description TEXT," +
+                    "unit_price REAL," +
+                    "image_resource_id INTEGER" +
+                    ");";
+
+            db.execSQL(sql);
+
+            insertOrder(db,"Lasanha", "lasanha de carne", 44.99, R.drawable.Lasanha);
+            insertOrder(db,"Macarrão", "macarrão a bolonhesa", 38.99, R.drawable.Macarrao);
+            insertOrder(db,"Porpeta", "porpeta ao molho", 29.99, R.drawable.Porpeta);
+        }
+    }
+}
